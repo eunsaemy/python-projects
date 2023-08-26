b65743ea3b340aa7a6afd98bff99973fb739a65f
Eunsaem Lee <eunsaemy@gmail.com>
2023-08-25 20:41:54 -0700

    add quiz (T/F) game

---
+++ quiz_game/data.py
@@ -0,0 +1,14 @@
+question_data = [
+    {
+        "text": "A slug's blood is green.",
+        "answer": "True",
+    },
+    {
+        "text": "The loudest animal is the African Elephant.",
+        "answer": "False",
+    },
+    {
+        "text": "Google was originally called 'Backrub'",
+        "answer": "True",
+    },
+]
